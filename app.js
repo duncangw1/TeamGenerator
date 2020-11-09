6e942f16eb15339f5e922de325c3d912c5c93858
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-11-09 16:44:10 -0500

    Built otherMembers function, createEngineer function, createIntern function, and createHTML function.

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ const myTeam = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+// Function to add the team manager's info and push to the myTeam array
 function teamManager() {
+  console.log("Please build your team");
   inquirer
     .prompt([
       {
@@ -47,7 +50,126 @@ function teamManager() {
       let newManager = new Manager(name, id, email, officeNumber);
       myTeam.push(newManager);
       console.log(myTeam);
+      // Calling otherMembers function to get info about the other team members
+      otherMembers();
     });
+}
+
+// Function that allows the user to choose which role the next team member has, then calls an appropriate function to gather specific info depending upon the role
+function otherMembers() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members",
+        ],
+        name: "otherMember",
+      },
+    ])
+    .then((data) => {
+      switch (data.otherMember) {
+        case "Engineer":
+          //placeholder for call to add engineer
+          createEngineer();
+          break;
+        case "Intern":
+          //placeholder for call to add intern
+          createIntern();
+          break;
+        case "I don't want to add any more team members":
+          //placeholder to call render function to generate the html
+          createHTML();
+          break;
+      }
+    });
+}
+
+// Function to gather info for the Engineer role
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's id?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's Github username?",
+        name: "github",
+      },
+    ])
+    .then((data) => {
+      let name = data.name;
+      let id = data.id;
+      let email = data.email;
+      let github = data.github;
+      let newEngineer = new Engineer(name, id, email, github);
+      myTeam.push(newEngineer);
+      console.log(myTeam);
+      otherMembers();
+    });
+}
+
+// Function to gather info for the Intern role
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your intern's id?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your intern's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your intern's school?",
+        name: "school",
+      },
+    ])
+    .then((data) => {
+      let name = data.name;
+      let id = data.id;
+      let email = data.email;
+      let school = data.school;
+      let newIntern = new Intern(name, id, email, school);
+      myTeam.push(newIntern);
+      console.log(myTeam);
+      otherMembers();
+    });
+}
+
+// Function to create new HTML file with the info gathered from the user
+function createHTML() {
+  let newHtml = render(myTeam);
+  console.log(newHtml);
+  fs.writeFile(outputPath, newHtml, function (err) {
+    if (err) return console.log(err);
+    console.log("Success! team.html has been created inside the output folder");
+  });
 }
 
 teamManager();
